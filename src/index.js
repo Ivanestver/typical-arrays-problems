bb4ed275decd6fe6560d537520e5200c92e38f87
Ivan <Ivanestver24@gmail.com>
2020-09-29 16:02:14 +0300

    feat: add logic for min

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let min = 1000000000;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+
+    return min;
+}
+
+exports.max = function max(array) {
   return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
   return 0;
 }
